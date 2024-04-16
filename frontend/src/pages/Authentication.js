388760ace29579f5d6c48878a24f8cfc2f679fe2
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-16 14:44:52 +0100

    Authform error response

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -8,19 +8,23 @@ export default AuthenticationPage;
 
 export async function action({ request }) {
   // built-in URL constructor, which is provided by the browser then access the searchParams object
+
+  //The searchParams object is part of the URL API 
   const searchParams = new URL(request.url).searchParams;
+
   // get is a method available in data
   const mode = searchParams.get('mode') || 'login';
   if (mode !== 'login' && mode !== 'signup') {
     throw json({ message: 'Unsupported mode' }, { status: 422 });
   }
+  // The formData() method comes from the Fetch API
   const data = await request.formData();
   const authData = {
     email: data.get('email'),
     password: data.get('password'),
   };
 
-  const response = fetch('http://localhost:8080/' + mode, {
+  const response = await fetch('http://localhost:8080/' + mode, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -31,6 +35,7 @@ export async function action({ request }) {
     return response;
   }
   if (!response.ok) {
+    // json is being called to create a JSON response.
     throw json({ message: 'Could not authenticate user' }, { status: 500 });
   }
 
