2c17acc76a3fe4c51189660e1196dd96df62eb9a
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-16 14:13:50 +0100

    authentication

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,37 @@
 import AuthForm from '../components/AuthForm';
+import { json } from 'react-router-dom';
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+  // built-in URL constructor, which is provided by the browser then access the searchParams object
+  const searchParams = new URL(request.url).searchParams;
+  // get is a method available in data
+  const mode = searchParams.get('mode') || 'login';
+  if (mode !== 'login' && mode !== 'signup') {
+    throw json({ message: 'Unsupported mode' }, { status: 422 });
+  }
+  const data = await request.formData;
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  };
+
+  const response = fetch('http://localhost:8080/' + mode, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(authData),
+  });
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+  if (!response.ok) {
+    throw json({ message: 'Could not authenticate user' }, { status: 500 });
+  }
+}
