1bc4c12dc8c2724163a1cb0e2fcc57566da5842d
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-16 18:13:09 +0100

    editting  auth

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -4,10 +4,11 @@ import {
   useNavigation,
   useActionData,
   json,
-  redirect
+  redirect,
 } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
+import { getAuthToken } from '../util/auth';
 
 function EventForm({ method, event }) {
   const data = useActionData();
@@ -84,6 +85,7 @@ function EventForm({ method, event }) {
 export default EventForm;
 
 export async function action({ request, params }) {
+  const token = getAuthToken();
   const method = request.method;
   const data = await request.formData();
 
@@ -105,6 +107,7 @@ export async function action({ request, params }) {
     method: method,
     headers: {
       'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + token,
     },
     body: JSON.stringify(eventData),
   });
@@ -119,4 +122,3 @@ export async function action({ request, params }) {
 
   return redirect('/events');
 }
-
